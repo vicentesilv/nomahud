58799bce9e8f4e09ddb61782529c5d5fd45ba3d5
vicentesilv <vicentesilvala@gmail.com>
2026-04-16 22:34:28 -0700

    coneccion a la base de datos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,20 +10,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'mysql',
-    //     host: configService.get("DB_HOST"),
-    //     port: configService.get("DB_PORT"),
-    //     username: configService.get("DB_USERNAME"),
-    //     password: configService.get("DB_PASSWORD"),
-    //     database: configService.get("DB_NAME"),
-    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //     synchronize: true,
-    //   }),
-    //   inject: [ConfigService],
-    // })
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: Number(configService.getOrThrow<string>('DB_PORT')),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        database: configService.getOrThrow<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
