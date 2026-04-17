542b0f11e987d24dd60e0c274a7cae430f0a5152
vicentesilv <vicentesilvala@gmail.com>
2026-04-16 19:33:02 -0700

    configurando el proyecto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    //carga de las variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'mysql',
+    //     host: configService.get("DB_HOST"),
+    //     port: configService.get("DB_PORT"),
+    //     username: configService.get("DB_USERNAME"),
+    //     password: configService.get("DB_PASSWORD"),
+    //     database: configService.get("DB_NAME"),
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: true,
+    //   }),
+    //   inject: [ConfigService],
+    // })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
